60801b55dd2c04e8cc1ade0d88ea35b9d69b4da1
WillTheVideoMan <williamhall1324@gmail.com>
2019-06-21 19:49:39 +0100

    Epic Beginnings.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const canvasRef = useRef();
+
+  const [mousePos, setMousePos] = useState({
+    x: 0,
+    y: 0,
+    lastX: 0,
+    lastY: 0
+  });
+
+  const [mouseDown, setMouseDown] = useState(false);
+
+  const handleMove = e => {
+    setMousePos({
+      x: e.layerX,
+      y: e.layerY,
+      lastX: mousePos.x,
+      lastY: mousePos.y
+    });
+  };
+
+  const handleDown = () => setMouseDown(true);
+
+  const handleUp = () => setMouseDown(false);
+
+  useEffect(() => {
+    const canvasInitial = canvasRef.current;
+    canvasInitial.addEventListener("mousemove", handleMove);
+    canvasInitial.addEventListener("mousedown", handleDown);
+    canvasInitial.addEventListener("mouseup", handleUp);
+    return () => {
+      canvasInitial.removeEventListener("mousemove", handleMove);
+      canvasInitial.removeEventListener("mousedown", handleDown);
+      canvasInitial.removeEventListener("mouseup", handleUp);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (mouseDown) {
+      const ctx = canvasRef.current.getContext("2d");
+      ctx.lineWidth = 8;
+      ctx.moveTo(0, 0);
+      ctx.lineTo(mousePos.x, mousePos.y);
+      ctx.stroke();
+    }
+  }, [mousePos]);
+
+  console.log(mousePos);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <canvas
+      style={{ border: "1px solid black" }}
+      width="280"
+      height="280"
+      ref={canvasRef}
+    />
   );
-}
+};
 
 export default App;
